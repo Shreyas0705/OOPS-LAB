46a928795dcfe949858ba4205c180277dbc412ac
Shreyas0705 <shreyas.7305@gmail.com>
2024-10-09 10:18:16 +0530

    Add files via upload

---
+++ Shreyas Srivastava 23FE10CSE00159/OOPs LAB 1.py
@@ -0,0 +1,28 @@
+msg="Hello Python"
+print(msg)
+name="Shreyas Srivastava"
+print(name)
+first_name="Shreyas"
+last_name="Srivastava"
+print(first_name)
+print(last_name)
+name="shreyas srivastava"
+print()
+print(name)
+print(name.title())
+print()
+
+print(f"Hello,{name.title()}!")
+a=input("enter your name")
+print(a.title())
+x=input("First Name")
+y=input("LAST NAME")
+print(f"{x}{y}")
+a=input("Enter Any Number")
+print(type(a))
+a=int(a)
+print(type(a))
+b=input("Enter another no.")
+print(a+b)
+c=a+b
+print(c)
